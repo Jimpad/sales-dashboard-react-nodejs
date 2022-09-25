839266b380cb4bb0b3155832d25ef481c94c3431
Dino Hadzic <hadzicdino99@gmail.com>
2022-09-25 23:42:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import '../src/components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <Dashboard>
+
+        </Dashboard>
+      </header>
+    </div>
+  );
+}
+
+export default App;
