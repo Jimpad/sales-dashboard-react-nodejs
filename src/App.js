bb66897dfc9e91d3eb2011fde3a442ac5983ca64
Dino Hadzic <hadzicdino99@gmail.com>
2022-09-25 23:56:47 +0200

    Comment appended to the App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import '../src/components/Dashboard';
 
 // Fix the import of Dashboard
+
+// Additionally switch this JS for TS
 function App() {
   return (
     <div className="App">
